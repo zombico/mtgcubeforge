a9005efc5ad1cd8a1627a45c3322ce8da5e033f8
zombico <franz@ombi.co>
2018-11-14 03:10:32 -0500

    confirm proper build

---
+++ src/components/ForgeCardObject.js
@@ -0,0 +1,62 @@
+const Forge = (reqst) => {
+  let reqstCard = {};
+  // left.side reqstCard is mine, reqst right.side is scryfall's
+  
+  // basic search case
+  if(reqst.layout ==="normal" || reqst.layout ==="saga") {
+  reqstCard.name = reqst.name;
+  reqstCard.type = reqst.type_line;
+  reqstCard.text = reqst.oracle_text;
+  reqstCard.layout = "normal";
+  reqstCard.imgsm = reqst.image_uris.small;
+  reqstCard.imgmd = reqst.image_uris.normal;  
+  }
+
+  // split card case
+  if(reqst.layout ==="split") {
+    reqstCard.name = reqst.card_faces[0].name;
+    reqstCard.nameFlip = reqst.card_faces[1].name
+    reqstCard.type = reqst.card_faces[0].type_line;
+    reqstCard.text = reqst.card_faces[0].oracle_text;
+    reqstCard.typeFlip = reqst.card_faces[1].type_line;
+    reqstCard.textFlip = reqst.card_faces[1].oracle_text;
+    reqstCard.layout = reqst.layout;
+    reqstCard.imgsm = reqst.image_uris.small;
+    reqstCard.imgmd = reqst.image_uris.normal;  
+    const aftermath = reqst.set.includes("akh") || reqst.set.includes("hou") ? true : false;
+    reqstCard.aftermath = aftermath;
+    // console.log(reqst.set)
+  }
+
+  // case for double faced cards
+  if (reqst.layout === "transform") {
+    reqstCard.name = reqst.card_faces[0].name;
+    reqstCard.nameFlip = reqst.card_faces[1].name
+    reqstCard.type = reqst.card_faces[0].type_line;
+    reqstCard.text = reqst.card_faces[0].oracle_text;
+    reqstCard.typeFlip = reqst.card_faces[1].type_line;
+    reqstCard.textFlip = reqst.card_faces[1].oracle_text;
+    reqstCard.layout = reqst.layout;
+    reqstCard.imgsm = reqst.card_faces[0].image_uris.small;
+    reqstCard.imgmd = reqst.card_faces[0].image_uris.normal
+    reqstCard.imgsmFlip = reqst.card_faces[1].image_uris.small;
+    reqstCard.imgmdFlip = reqst.card_faces[1].image_uris.normal    
+  }
+
+  // case for flip cards
+  if (reqst.layout === "flip") {
+    reqstCard.name = reqst.card_faces[0].name;
+    reqstCard.nameFlip = reqst.card_faces[1].name
+    reqstCard.type = reqst.card_faces[0].type_line;
+    reqstCard.text = reqst.card_faces[0].oracle_text;
+    reqstCard.typeFlip = reqst.card_faces[1].type_line;
+    reqstCard.textFlip = reqst.card_faces[1].oracle_text;
+    reqstCard.layout = reqst.layout;
+    reqstCard.imgsm = reqst.image_uris.small;
+    reqstCard.imgmd = reqst.image_uris.normal;
+  }
+  
+  return reqstCard
+}
+
+export default Forge;
---
+++ src/components/SearchCard.js
@@ -0,0 +1,257 @@
+import React, { Component } from 'react';
+import Forge from './ForgeCardObject';
+
+class SearchCard extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: '',
+      tempCard: '',
+      stateReqstCard: '',
+      autoQueryIn: '',
+      autoQueryOut: '',
+      currentSearchRank: -1
+    }
+    this.focusCard = this.focusCard.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getCard = this.getCard.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.autoComplete = this.autoComplete.bind(this);
+    this.checkIfOne = this.checkIfOne.bind(this);
+    this.closeSuggestions = this.closeSuggestions.bind(this);
+
+    this.moveKey = this.moveKey.bind(this);
+    this.firstSearch = this.firstSearch.bind(this);
+    this.firstResult = React.createRef();    
+  }
+
+  
+  // Component flow
+  //  search is typed on input. autoComplete(term) runs on event change, and chains methods to clean up results
+  //  when there is only one card in autoComplete results, the getCard(card) method is called and saves the result object to state.tempCard
+  //  if user decides to add the card, on submit, Forge() is called to  take relevant lines, and is assigned to stateReqstCard
+
+
+  autoComplete(term) {  
+    fetch(`https://api.scryfall.com/cards/autocomplete/?q=${term}`, {
+    })
+    .then(res => res.json())
+    .then(result => {
+      // console.log(result)
+      this.setState({
+        autoQueryIn: result.data
+      })
+    }).then(() => {
+      const cardy = this.state.autoQueryIn;
+      if (cardy !== "" ) {
+        this.checkIfOne(cardy, term)
+        const termLength = term.length > 2 ? term.length : 0;
+        const searchArrayOnly = cardy.map((cardName) => {
+          const checker = cardName.substring(0, termLength)
+          // console.log(checker)
+          if (checker.toLowerCase() === term.toLowerCase()) {
+            return cardName        
+          }
+          else if (cardName.includes("//") && cardName.charAt(0) === term.charAt(0)) { 
+            return cardName 
+          }
+          else if (cardName.includes(",") && cardName.charAt(1) === term.charAt(1)) {
+            return cardName
+          }
+          else if (cardName.charAt(1) === term.charAt(1) && cardName.charAt(0) === term.charAt(0)) {
+            return cardName
+          }
+          else return "butts"
+        })
+        const cleanArray = searchArrayOnly.filter(name => name !== "butts").sort()
+        console.log(cleanArray)
+        this.setState({ autoQueryOut: cleanArray })
+        if (cleanArray.length === 1) {
+          this.getCard(cleanArray[0]);        
+        }
+      }  
+    })
+    
+    
+  }
+
+  getCard(card) {
+    fetch(`https://api.scryfall.com/cards/named?exact=${card}`, {
+    })
+    .then(res => res.json())
+    .then(result => {  
+      this.setState({
+        tempCard: result,
+        
+      }) })
+    .then(() => {
+      this.focusCard()
+    })
+    .catch(error => console.error('Error', error))
+  }
+
+  focusCard() {
+    const reqstCard = Forge(this.state.tempCard);
+    this.setState({
+      stateReqstCard: reqstCard,
+    })
+  }
+
+  closeSuggestions(card) {
+    this.setState({
+      autoQueryOut: [],
+      searchTerm: card
+    })
+  }
+
+  checkIfOne(array, searchTerm) {
+    array.forEach((result) => {
+      let exactoChecker = result.length;  
+      if (exactoChecker === searchTerm.length) {
+        this.getCard(searchTerm)
+      }
+    });
+  }
+
+  handleAdd(event) {
+    event.preventDefault();
+    this.focusCard();
+  }
+
+  handleChange(event) {
+    const newStroke = event.target.value    
+    this.setState({
+      searchTerm: newStroke
+    })
+    this.autoComplete(newStroke)
+  }
+
+
+  moveKey(e) {
+    const cardList = this.state.autoQueryOut;
+    const searchOrder = cardList.length + 1
+    const searchRank = this.state.currentSearchRank +1
+    console.log(searchOrder, searchRank)
+    const onLastResult = searchOrder - searchRank === 1 ? true : false
+    const onFirstResult = searchOrder - searchRank === searchOrder ? true : false
+
+    // focus on the correct index when pressing down key
+    if (e.key === "ArrowDown" && !onLastResult ) {
+      e.preventDefault();
+      const newRank = searchRank
+      console.log(newRank)
+      this.setState({currentSearchRank: newRank}, () => {
+      const searchIndex = this.state.currentSearchRank;
+      document.getElementById(`searchresult${searchIndex}`).focus()
+      })  
+    // stop thing from breaking when getting to last
+    } else if (e.key === "ArrowDown" && onLastResult) {
+      e.preventDefault();
+      console.log('last')
+    }
+
+    // focus on correct index when pressing up key
+    else if ( e.key === "ArrowUp" && !onFirstResult ) {
+      e.preventDefault();
+      console.log('i pressed up')
+      const newRank = this.state.currentSearchRank - 1
+      this.setState({currentSearchRank: newRank})
+      const searchIndex = this.state.currentSearchRank;
+      document.getElementById(`searchresult${searchIndex}`).focus() 
+    } 
+    // Go back to input when you reach top of list
+    else if (e.key === "ArrowUp" && onFirstResult) {
+      e.preventDefault();
+      document.getElementById("searchcard").focus()
+      this.setState({currentSearchRank: -1})
+    }
+    // Stop the component from previewing when hit bottom of list
+    else if ( e.key === "ArrowDown" && searchOrder === searchRank) {
+      e.preventDefault();
+    }
+    // Delete first letter of search when pressing delete key on button
+    else if ( e.keyCode === 8) {
+      e.preventDefault();
+      document.getElementById("searchcard").focus()
+      const term = this.state.searchTerm;
+      const termLength = term.length -1;
+      const backSpacedTerm = term.substring(0, termLength)
+      this.setState({searchTerm: backSpacedTerm})
+    } 
+    
+  }
+
+  firstSearch(e) {
+    const cardList = this.state.autoQueryOut;
+    const searchOrder = cardList.length
+    if (e.key === "ArrowDown" && searchOrder > 0 && cardList.length !== 0) {
+      e.preventDefault();
+      this.setState({currentSearchRank: 0})
+      document.getElementById("searchresult0").focus()
+    }
+    
+  }
+  
+  render() {
+    const card = this.state.stateReqstCard
+    const suggestions = this.state.autoQueryOut;
+    const searchTerm = this.state.searchTerm;
+    
+    return (
+      <div className="searchbar">  
+        <div className="leftside">
+        <form onSubmit={this.handleAdd}>
+          <button>Add</button>
+        </form>
+        
+        { card.layout === "normal" &&
+          <img alt="" className="preview-img-med" src={card.imgmd} />
+        }
+        { card.aftermath &&
+          <img alt="" className="preview-img-med aftermath" src={card.imgmd} />
+        }
+        { card.layout === "split" && !card.aftermath &&
+          <img alt="" className="preview-img-med split" src={card.imgmd} />
+        }
+        { card.layout === "flip" &&
+          <img alt="" className="preview-img-med flip" src={card.imgmd} />
+        }
+        { card.layout === "transform" && 
+        <div className="searchbar__images">
+          <img alt="" className="preview-img-med dfc" src={card.imgmd} /> 
+          <img alt="" className="preview-img-med dfc" src={card.imgmdFlip} /> 
+        </div> 
+        }
+        </div>
+        <div className="rightside">
+          <div className="searchbar__menu">
+          
+          <input className="searchbar__input" type="text" id="searchcard"
+            value={searchTerm} onChange={this.handleChange}
+            onKeyDown={(e) => this.firstSearch(e)}
+          />
+          {  
+            suggestions.length !==0 && searchTerm.length > 2 ? suggestions.map((card, index) => 
+            
+            <button 
+              onKeyDown={(e) => this.moveKey(e)}
+              ref={index === 0 && this.firstResult } 
+              id={"searchresult"+index}  key={index} 
+              className="searchbar__autoresult" 
+              onMouseEnter={() => this.getCard(card)}
+              onFocus={() => this.getCard(card)} 
+              onClick={() => this.closeSuggestions(card)}
+            >
+                {card}
+            </button>
+            ) : '' 
+          }        
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchCard;
