3176cbd06235d9254f03297cd9bf4a2e7c0fd1c4
Franz <franz@mbps-MacBook-Pro.local>
2018-12-14 01:01:11 -0500

    add color to reducer logic

--- api/seeds/index.js
+++ api/seeds/index.js
@@ -7,18 +7,18 @@ const cubes = require('./cubes')
 const uri = 'mongodb://localhost:27017/cubeforge'
 
 const truncateDatabase = async () => {
-  // here we delete all our users and posts so we can start with fresh data
   return Promise.all([User.deleteMany(), Cube.deleteMany()])
 }
 
 const makeSeeds = async () => {
   
   await mongoose.connect(uri)
-  // delete all old data in the database
+  
   await truncateDatabase()
   
   await Promise.all(users.map(user => user.save()))
   const docs = await User.find({})
+  
   // turn on Fulgren's cube
   cubes[0].userId = docs[0]._id
   
