d57079a15dfc40552d1ea83c3132c47f51a76827
Franz <franz@mbps-MacBook-Pro.local>
2018-12-14 23:23:32 -0500

    backend jwt token

---
+++ api/middleware/auth.js
@@ -0,0 +1,18 @@
+const tokenService = require('../utils/tokenService')
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.get('Authorization')
+
+  if (!authHeader) {
+    return next(new Error('unauthorized'))
+  }
+  // split the Bearer from the token
+  const token = authHeader.split(' ')[1]
+  try {
+    const decoded = await tokenService.verify(token)
+    req.token = decoded
+    next()
+  } catch (e) {
+    next(new Error('unauthorized'))
+  }
+}
--- api/server.js
+++ api/server.js
@@ -12,6 +12,7 @@ app.use('/healthcheck', require('./routes/index').router);
 app.use('/login', require('./routes/login').router);
 app.use('/fulgrens_cube', require('./routes/fulgrens_cube').router);
 app.use('/signup', require('./routes/signup').router);
+app.use('/login', require('./routes/login').router);
 app.use('/users', require('./routes/users').router);
 app.use('/cubes', require('./routes/cubes').router);
 app.use('/newcube', require('./routes/newcube').router);
