0ca2d464c4a3434ae9e4ca341a7c7828372bcd3f
Franz <franz@mbps-MacBook-Pro.local>
2018-12-16 17:05:54 -0500

    updated mongo connection string

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -2,4 +2,4 @@
 
 exports.PORT = process.env.PORT || 3001;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
-exports.DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/cubeforge';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/cubeforge';
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 const mongoose = require('mongoose')
 const server = require('./api/server');
-const { PORT, DB_URI } = require('./api/utils/constants');
+const { PORT, MONGODB_URI } = require('./api/utils/constants');
 
 server.listen(PORT, async () => {
-  await mongoose.connect(DB_URI)
-  console.log(`Database connected at ${DB_URI}`)
+  await mongoose.connect(MONGODB_URI)
+  console.log(`Database connected at ${MONGODB_URI}`)
   console.log(`App listening on port ${PORT}`)
 });
