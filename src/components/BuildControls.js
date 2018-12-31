589ed6e2f9828c6392d32e07c6e217d3bf533a99
Franz <franz@mbps-MacBook-Pro.local>
2018-12-30 21:32:26 -0500

    stable searchcard shortcuts

--- src/components/BuildControls.js
+++ src/components/BuildControls.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 
 class BuildControls extends Component {
+  colorShortcut(color, e) {
+    e.preventDefault()
+    document.getElementById(`${color}section`).scrollIntoView();
+  }
+  
   render() {
     return (
       <div className="searchbar__buildcontrols">
         <div className="searchbar__buildcontrols__container">
-          <form onSubmit={this.props.handleAdd}>
+          <form className="searchbar__buildcontrols__mainbutton" onSubmit={this.props.handleAdd}>
             
             {this.props.stateReqstCard && <button className="addtocube" id="addtocube">Add to Cube</button> }
             {!this.props.stateReqstCard && <button className="searchbar__buildcontrols-shim">hi</button>}
             {!this.props.stateReqstCard && <button disabled className="addtocube disabled">Add to Cube</button> }
+            
           </form>
+          <div className="searchbar__buildcontrols__shortcut" >
+            <div className="searchbar__buildcontrols__label">Jump to</div>
+            <a href="#" onClick={(e) => this.colorShortcut('multicolor', e)}>Multicolor</a>
+            <a href="#" onClick={(e) => this.colorShortcut('blue', e)}>Blue</a>
+            <a href="#" onClick={(e) => this.colorShortcut('black', e)}>Black</a>
+            <a href="#" onClick={(e) => this.colorShortcut('white', e)}>White</a>
+            <a href="#" onClick={(e) => this.colorShortcut('green', e)}>Green</a>
+            <a href="#" onClick={(e) => this.colorShortcut('colorless', e)}>Colorless</a>
+            <a href="#" onClick={(e) => this.colorShortcut('land', e)}>Land</a>
+          </div>  
         </div>
+        
       </div>
     )
   }
